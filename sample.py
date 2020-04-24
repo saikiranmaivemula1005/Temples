de48a876c96626001cbb5a8e0e7ec19c655f419e
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2020-04-24 20:58:13 +0530

    sample python file

---
+++ sample.py
@@ -0,0 +1,5 @@
+Tirupati is a temple of Venkateswara (Vishnu), located in Chittoor District, AP, India.
+The temple is believed to be constructed around 300 AD.
+The temple is managed by Tirumala Tirupati Devasthanams(TTD).
+The local language is Telugu.
+Find more information in the official website www.tirumala.org 
